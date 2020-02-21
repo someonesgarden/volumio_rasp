5ebd89f63e8b55250e1f2b57e33ab044fa4fc651
Daisuke Nishimura <d@someonesgarden.org>
2020-02-22 00:22:09 +0900

    gpioToVolumio fixed to react button

--- gpioToVolumio.js
+++ gpioToVolumio.js
@@ -4,25 +4,17 @@ let gpio = require('rpi-gpio');
 
 let LED_RED     = 19; // GPIO10
 let LED_GREEN   = 21; // GPIO9
-let LED_BLUE    = 23; // GPIO11
-
-let volume      = 0;
-let vol_delta   = 1;
-
+//let LED_BLUE    = 23; // GPIO11
 const BTN_VOL_DOWN    = 36; //GPIO16  VOLUME UP
 const BTN_PLAY_TOGGLE = 33; //GPIO13 VOLUME DOWN
 const BTN_VOL_UP      = 37; //GPIO26 PLAYTOGGLE
 
-
 const LED_BLINK_DELAY_MS = 1000;
+let volume      = 0;
+let vol_delta   = 1;
 
-let led1On = true;
-let led2On = true;
-let led3On = true;
 
 process.stdout.write("test");
-
-console.log("gpioToVolumio:INIT");
 socket.emit('getState', '');
 let status = "null";
 
@@ -62,15 +54,8 @@ gpio.on('change', (ch, value) => {
 
                 if(status==='play'){
                     socket.emit('pause');
-                    gpio.write(LED_GREEN, true);
-                    gpio.write(LED_RED, true);
-                    gpio.write(LED_BLUE, false);
-                    led1On = true;
                 }else{
                     socket.emit('play');
-                    gpio.write(LED_BLUE, true);
-                    gpio.write(LED_RED, false);
-                    gpio.write(LED_GREEN, false);
                 }
                 break;
         }
@@ -80,31 +65,26 @@ gpio.on('change', (ch, value) => {
 
 
 
-// gpio.setup(LED_RED, gpio.DIR_OUT, () => {
-//     setInterval(() => {
-//         if(led1On) {
-//             gpio.write(LED_RED, false);
-//             led1On = false;
-//         } else {
-//
-//             gpio.write(LED_RED, true);
-//             led1On = true;
-//         }
-//     },LED_BLINK_DELAY_MS);
-// });
-//
-// gpio.setup(LED_GREEN, gpio.DIR_OUT, () => {
-//     setInterval(() => {
-//         if(led2On) {
-//             gpio.write(LED_GREEN, false);
-//             led2On = false;
-//         } else {
-//             gpio.write(LED_GREEN, true);
-//             led2On = true;
-//         }
-//     },LED_BLINK_DELAY_MS*2);
-// });
-//
+gpio.setup(LED_RED, gpio.DIR_OUT, () => {
+    setInterval(() => {
+        if(status==='play') {
+            gpio.write(LED_RED, false);
+        } else if(status==='pause'){
+            gpio.write(LED_RED, true);
+        }
+    },LED_BLINK_DELAY_MS);
+});
+
+gpio.setup(LED_GREEN, gpio.DIR_OUT, () => {
+    setInterval(() => {
+        if(status==='play'){
+            gpio.write(LED_GREEN, true);
+        }  else if(status==='pause'){
+            gpio.write(LED_GREEN, false);
+        }
+    },LED_BLINK_DELAY_MS*2);
+});
+
 // gpio.setup(LED_BLUE, gpio.DIR_OUT, () => {
 //     setInterval(() => {
 //         if(led3On) {
